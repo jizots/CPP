89f83996ff9d0dc844f1ff8e2eaf71e716c75923
sota <hotphilly1776@yahoo.co.jp>
2023-12-30 14:33:34 +0900

    Draft m2/ex01

---
+++ m2/ex01/srcs/Fixed.cpp
@@ -0,0 +1,35 @@
+#include "Fixed.hpp"
+
+Fixed::Fixed(void)
+{
+	std::cout << "Default constructor called" << std::endl;
+	setRawBits(0);
+}
+
+Fixed::Fixed(const Fixed& other)
+{
+	std::cout << "Copy constructor called" << std::endl;
+	setRawBits(other.getRawBits());
+}
+
+Fixed& Fixed::operator=(const Fixed& rhs)
+{
+	std::cout << "Copy assignment operator called" << std::endl;
+	m_fixed_point = rhs.getRawBits();
+	return (*this);
+}
+
+int	Fixed::getRawBits(void) const
+{
+	std::cout << "getRawBits member function called" << std::endl;
+	return (m_fixed_point);
+}
+
+void	Fixed::setRawBits(int const raw)
+{
+	m_fixed_point = raw;
+}
+Fixed::~Fixed(void)
+{
+	std::cout << "Destructor called" << std::endl;
+}
---
+++ m2/ex01/srcs/Fixed.hpp
@@ -0,0 +1,26 @@
+#ifndef FIXED_HPP
+# define FIXED_HPP
+
+# include <string>
+# include <iostream>
+# include <cmath>
+
+class Fixed
+{
+public:
+	Fixed(void);
+	Fixed(const Fixed& other);
+	Fixed(const int num);
+	Fixed(const float num);
+	~Fixed(void);
+	Fixed& operator=(const Fixed& rhs);
+	int		getRawBits(void) const;
+	void	setRawBits(int const raw);
+	float	toFloat( void ) const;
+
+private:
+	int					m_fixed_point;
+	static const int	m_frac_bits = 8;
+};
+
+#endif
