38388b36cce77d9b668991a75ae05a3472014b00
sota <hotphilly1776@yahoo.co.jp>
2024-01-02 12:38:10 +0900

    draft m2/ex02

---
+++ m2/ex02/srcs/Fixed.cpp
@@ -0,0 +1,90 @@
+#include "Fixed.hpp"
+
+Fixed::Fixed(void)
+{
+	std::cout << "Default constructor called" << std::endl;
+	setRawBits(0);
+}
+
+Fixed::Fixed(const Fixed& other)
+{
+	std::cout << "Copy constructor called" << std::endl;
+	*this = other;
+}
+
+Fixed::Fixed(const int num)
+{
+	std::cout << "Int constructor called" << std::endl;
+	if (8388607 < num)
+	{
+		std::cout << "Error: Overflow. Set below 8388607." << std::endl;
+		exit(1);
+	}
+	else if (num < -8388608)
+	{
+		std::cout << "Error: Underflow. Set above -8388608." << std::endl;
+		exit(1);
+	}
+	setRawBits(num * (1 << m_frac_bits));
+}
+
+/*0.00390625 ~ 0.99609375は'0.00390625'刻みの範囲で表現できる。*/
+Fixed::Fixed(const float num)
+{
+	int				shift;
+	unsigned int	exponent = m_frac_bits;
+
+	std::cout << "Float constructor called" << std::endl;
+	shift = 1;
+	while (exponent--)
+		shift *= 2;
+	setRawBits(static_cast<int>(roundf(num * shift)));
+}
+
+Fixed& Fixed::operator=(const Fixed& rhs)
+{
+	std::cout << "Copy assignment operator called" << std::endl;
+	setRawBits(rhs.m_fixed_point);
+	return (*this);
+}
+
+int	Fixed::getRawBits(void) const
+{
+	std::cout << "getRawBits member function called" << std::endl;
+	return (m_fixed_point);
+}
+
+void	Fixed::setRawBits(int const raw)
+{
+	m_fixed_point = raw;
+}
+
+Fixed::~Fixed(void)
+{
+	std::cout << "Destructor called" << std::endl;
+}
+
+float	Fixed::toFloat( void ) const
+{
+	float	ret;
+	int		shift;
+	unsigned int	exponent = m_frac_bits;
+
+	ret = static_cast<float>(this->m_fixed_point);
+	shift = 1;
+	while (exponent--)
+		shift *= 2;
+	ret = ret / shift;
+	return (ret);
+}
+
+int		Fixed::toInt( void ) const
+{
+	int				shift;
+	unsigned int	exponent = m_frac_bits;
+
+	shift = 1;
+	while (exponent--)
+		shift *= 2;
+	return (roundf(m_fixed_point / shift));
+}
---
+++ m2/ex02/srcs/Fixed.hpp
@@ -0,0 +1,33 @@
+#ifndef FIXED_HPP
+# define FIXED_HPP
+
+# include <string>
+# include <iostream>
+# include <cmath>
+
+class Fixed
+{
+public:
+	Fixed(void);
+	Fixed(const Fixed& other);
+	Fixed(const int num);
+	Fixed(const float num);
+	~Fixed(void);
+	Fixed&			operator=(const Fixed& rhs);
+	int				getRawBits(void) const;
+	void			setRawBits(int const raw);
+	float			toFloat( void ) const;
+	int				toInt( void ) const;
+	static	Fixed&	min(Fixed& a, Fixed& b);
+	static	Fixed&	min(const Fixed& a, const Fixed& b);
+	static	Fixed&	max(Fixed& a, Fixed& b);
+	static	Fixed&	max(const Fixed& a, const Fixed& b);
+
+private:
+	int					m_fixed_point;
+	static const int	m_frac_bits = 8;
+};
+
+std::ostream& operator<<(std::ostream& os, const Fixed& rhs);
+
+#endif
