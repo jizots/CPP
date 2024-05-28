6344a6d7a804818fc7328cedbdf8d7d6a595f694
jizots <hotphilly1776@yahoo.co.jp>
2024-05-28 15:07:50 +0900

    [wip] before integrateToMainChain templated

--- m9/ex02/srcs/PmergeMe.hpp
+++ m9/ex02/srcs/PmergeMe.hpp
@@ -24,7 +24,7 @@ public:
 
 	typedef std::deque<uint32> deqContainer;
 	typedef std::pair<uint32, deqContainer::const_iterator> pairMainChainDeq;
-	typedef std::vector< pairMainChainDeq > vecMainChainDeq;
+	typedef std::deque< pairMainChainDeq > deqMainChain;
 
 public:
 	PmergeMe(int ac, char** argv);
@@ -36,8 +36,33 @@ public:
 	deqContainer& getContainerDeque(void);
 	const deqContainer& getContainerDeque(void) const;
 
-	void mergeInsertionSortVec(const vecContainer::size_type chunkScale, 
-		const vecContainer::size_type chunkSize, const bool hasRemainder);
+	template <typename TContainer>
+	void mergeInsertionSortVec(TContainer& data, const vecContainer::size_type chunkScale,
+		const vecContainer::size_type chunkSize, const bool hasRemainder)
+	{
+		if (chunkSize < 1)
+			return ;
+		// ここでchunkScale内のbiggest要素をとなりのchunkと比較して小大の順に並べる
+		compareChunkAndSwap(data, chunkScale);
+		# ifdef DEBUG
+			std::cout << "### After compare Chunk ###" << std::endl;
+			printContainer(m_containerVec);
+		# endif //DEBUG
+		// 再帰から帰ってくると、chunkSize + 1の要素が降順に並んだ状態になっている
+		mergeInsertionSortVec(data, chunkScale * 2, chunkSize / 2, chunkSize % 2);
+		// chunkSize * 2の要素数を、一列に並べる。
+		if (1 < chunkScale)
+			integrateToMainChain(data, chunkScale, chunkSize, (hasRemainder ? 1 : 0));
+		# ifdef DEBUG
+			if (chunkScale == 1)
+			{
+				std::cout << "compare count: " << m_compareCount << std::endl;
+				m_compareCount = 0;
+				std::cout << std::endl;
+				std::cout << std::endl;
+			}
+		# endif //DEBUG
+	};
 
 private:	
 	PmergeMe(void);
@@ -60,18 +85,87 @@ public:
 
 private:
 	void addContainerByArgs(int ac, char** argv);
-	void compareChunkAndSwap(const vecContainer::size_type chunkScale);
-	bool isLittleLeftChunk(const vecContainer::size_type iLeftChunkEnd,
-		const vecContainer::size_type iRightChunkEnd);
-	void swapChunk(const vecContainer::size_type chunkScale,
-		const vecContainer::size_type iLeftChunkStart, const vecContainer::size_type iRightChunkStart);
-	vecContainer::size_type getNextIntegratePos(const vecContainer::size_type number);
-	std::vector< pairMainChain > makeTempMainChain(const vecContainer::size_type chunkScale);
-	void integrateToMainChain(const vecContainer::size_type chunkScale,
+	vecMainChain makeTempMainChain(const vecContainer& data, const vecContainer::size_type chunkScale);
+	deqMainChain makeTempMainChain(const deqContainer& data, const deqContainer::size_type chunkScale);
+	void integrateToMainChain(vecContainer& data, const vecContainer::size_type chunkScale,
 		const vecContainer::size_type chunkSize, int hasRemainder);
-	const vecMainChain::const_iterator recursiveSearchInsertPos(const vecMainChain::const_iterator begin,
-		const vecMainChain::const_iterator end, const vecMainChain::value_type targetVal);
-	vecContainer buildContainerFromMainchain(const vecContainer::size_type& chunkScale, const vecMainChain& mainChain);
+	vecContainer buildContainerFromMainchain(const vecContainer& data,
+		const vecContainer::size_type& chunkScale, const vecMainChain& mainChain);
+	deqContainer buildContainerFromMainchain(const deqContainer& data,
+		const deqContainer::size_type& chunkScale, const deqMainChain& mainChain);
+
+	template <typename TContainer>
+	void compareChunkAndSwap(TContainer& data, const typename TContainer::size_type chunkScale)
+	{
+		// iがさすのは、比較元（chunkLeft）の開始位置
+		for (typename TContainer::size_type i = 0; i + (chunkScale * 2) <= data.size(); i += (chunkScale * 2))
+		{
+			if (!isLittleLeftChunk(data, i + (chunkScale - 1), i + (chunkScale * 2) - 1))
+				swapChunk(data, chunkScale, i, i + chunkScale);
+		}
+	}
+
+	template <typename TContainer>
+	bool isLittleLeftChunk(TContainer& data, const typename TContainer::size_type iLeftChunkEnd,
+		const typename TContainer::size_type iRightChunkEnd)
+	{
+		# ifdef DEBUG
+			std::cout << "compareChunk: " << data[iLeftChunkEnd] << " vs " 
+				<< data[iRightChunkEnd] << std::endl;
+		# endif //DEBUG
+		++m_compareCount;
+		if (data[iLeftChunkEnd] <= data[iRightChunkEnd])
+			return (true);
+		return (false);
+	}
+
+	template <typename TContainer>
+	void swapChunk(TContainer& data, const typename TContainer::size_type chunkScale,
+			const typename TContainer::size_type iLeftChunkStart, const typename TContainer::size_type iRightChunkStart)
+	{
+		TContainer copyRight(data.begin() + iRightChunkStart, data.begin() + iRightChunkStart + chunkScale);
+
+		data.erase(data.begin() + iRightChunkStart, data.begin() + iRightChunkStart + chunkScale);
+		data.insert(data.begin() + iLeftChunkStart, copyRight.begin(), copyRight.end());
+	}
+
+	template <typename TContainer>
+	typename TContainer::size_type getNextIntegratePos(const typename TContainer::size_type number)
+	{
+		return ((std::pow(2, number + 1) + std::pow(-1, number)) / 3);
+	}
+
+	template <typename TMainChain>
+	const typename TMainChain::const_iterator recursiveSearchInsertPos(const typename TMainChain::const_iterator begin,
+		const typename TMainChain::const_iterator end, const typename TMainChain::value_type targetVal)
+	{
+		if (begin == end)
+			return (begin);
+		else if ((end - begin) == 1)
+		{
+			++m_compareCount;
+
+			# ifdef DEBUG
+				std::cout << "compare: " << (*begin).first << " vs " << targetVal.first << std::endl;
+			# endif //DEBUG
+			if (targetVal.first < (*begin).first)
+				return (begin);
+			else
+				return (begin + 1);
+		}
+		else
+		{
+			++m_compareCount;
+			const typename TMainChain::const_iterator middle = begin + ((end - begin) / 2);
+			# ifdef DEBUG
+				std::cout << "compare: " << (*middle).first << " vs " << targetVal.first << std::endl;
+			# endif //DEBUG
+			if (targetVal < *middle)
+				return (recursiveSearchInsertPos<TMainChain>(begin, middle, targetVal));
+			else
+				return (recursiveSearchInsertPos<TMainChain>(middle + 1, end, targetVal));
+		}
+	}
 
 	template <typename T>
 	static bool isUnsigned(){ return (false); };
